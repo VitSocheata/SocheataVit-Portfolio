418e8e902f2734e27c871db93ca7cc643715e97a
Vit Socheata <145731923+VitSocheata@users.noreply.github.com>
2026-07-30 16:10:32 +0700

    add off inspect

--- src/App.vue
+++ src/App.vue
@@ -9,4 +9,21 @@
 <script setup>
 import Navbar from '@/components/layout/Navbar.vue';
 import Footer from '@/components/layout/FooTer.vue';
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault()
+  })
+
+  document.addEventListener('keydown', (e) => {
+    if (
+      e.key === 'F12' || 
+      (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J' || e.key === 'C')) || 
+      (e.ctrlKey && e.key === 'U')
+    ) {
+      e.preventDefault()
+    }
+  })
+})
 </script>
