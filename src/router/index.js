2355d8b4e552d856a74bc696da9a20ec05f906d0
Vit Socheata <145731923+VitSocheata@users.noreply.github.com>
2026-08-06 08:40:31 +0700

    add seo to my portfolio

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,12 @@ const router = createRouter({
         {
           path: '',
           name: 'home',
-          component: HomeView
+          component: HomeView,
+          meta: {
+            title: 'Vit Socheata - Frontend Developer Portfolio',
+            description: 'Explore the professional portfolio of Vit Socheata, a Frontend Developer and Computer Science student.'
+          }
+
         }
       ]
     },
@@ -32,6 +37,18 @@ const router = createRouter({
     }
     return { top: 0, behavior: 'smooth' };
   }
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title;
+  }
+  let metaTag = document.querySelector('meta[name="description"]');
+  if (metaTag && to.meta.description) {
+    metaTag.setAttribute('content', to.meta.description);
+  }
+  
+  next();
 });
 
 export default router;
