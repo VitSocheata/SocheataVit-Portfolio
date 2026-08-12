f1a4f42b0502567fb2d4e1f40beb63115a14cfcb
Vit Socheata <145731923+VitSocheata@users.noreply.github.com>
2026-08-12 10:39:51 +0700

    update contact

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,7 +15,7 @@ import MyWork from '@/components/home/WorkHistory.vue';
 import EducationSection from '@/components/home/EducationSection.vue';
 import SkillSet from '@/components/home/SkillSet.vue';
 import Portfolio from '@/components/home/Portfolio.vue';
-import Contact from '@/components/home/Contact.vue';
+import Contact from '@/components/home/Contacts.vue';
 import AboutMe from '@/components/home/AboutMe.vue';
 
 </script>
